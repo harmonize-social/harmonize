a5f385a6bc111ce29f2e63d405704774af10fb59
Adil11000 <133091835+Adil11000@users.noreply.github.com>
2023-12-12 11:51:37 +0100

    component textinput

---
+++ frontend/src/components/TextInput.svelte
@@ -0,0 +1,32 @@
+<script>
+    export let value = '';
+    export let placeholder = '';
+    export let disabled = false;
+  
+    function handleChange(event) {
+      value = event.target.value;
+    }
+  </script>
+  
+  <input
+    type="text"
+    bind:value={value}
+    placeholder={placeholder}
+    {disabled}
+    on:input={handleChange}
+  />
+
+<!--
+<script>
+  import TextInput from './TextInput.svelte';
+  
+  let inputValue = ''; // Je kunt deze variabele gebruiken om de waarde bij te houden.
+
+  function handleInputChange(value) {
+    console.log('Nieuwe waarde:', value);
+  }
+</script>
+
+<TextInput bind:value={inputValue} placeholder="Voer tekst in" on:input={handleInputChange} />
+-->
+
