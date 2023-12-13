8eee14360889da09685e577ffa130960dbedc824
SimonVerbandt <101420997+SimonVerbandt@users.noreply.github.com>
2023-12-13 10:28:43 +0100

    Update +page.svelte

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,7 +1,2 @@
-<script>
-    import Post from "../components/Post.svelte";
-</script>
-
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-<Post></Post>
