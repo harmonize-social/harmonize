78668905bbce1027f36a8220a5a656b30f82a032
Matthew Vercammen <91327198+matthew-vercammen@users.noreply.github.com>
2023-12-13 13:06:00 +0100

    created model.go & handler.go

--- backend/go.mod
+++ backend/go.mod
@@ -1,3 +1,10 @@
 module backend
 
 go 1.21.5
+
+require (
+	github.com/gorilla/mux v1.8.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+
+)
--- backend/internal/models/model.go
+++ backend/internal/models/model.go
@@ -1,1 +1,8 @@
 package models
+
+type User struct {
+    ID       int64  `json:"id"`
+	Email string `json:"email"`
+    Username     string `json:"username"`
+    Password string  `json:"password_hash"`
+}
