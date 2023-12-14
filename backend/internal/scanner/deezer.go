80ee728469d9def372cedadf8ae279b2e36747f3
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2023-12-14 21:19:06 +0100

    fetched albums

--- backend/internal/scanner/deezer.go
+++ backend/internal/scanner/deezer.go
@@ -33,6 +33,25 @@ func ScanDeezerUserPlaylists(id int) {
     }
 }
 
-func ScanDeezerUserAlbums(id int)  {}
+func ScanDeezerUserAlbums(id int) {
+    index := 0
+    limit := 5
+    var all []deezer.Album
+    for len(all) >= index {
+        list, err := deezer.GetUserAlbums(id, index, limit)
+        if err != nil {
+            fmt.Printf("%s", err)
+            break
+        }
+        for _, playlist := range list {
+            all = append(all, playlist)
+        }
+        index += limit
+    }
+    for _, playlist := range all {
+        fmt.Printf("%s\n\r", playlist.Title)
+    }
+}
+
 func ScanDeezerUserTracks(id int)  {}
 func ScanDeezerUserArtists(id int) {}
