7529f302781b98f741629f18756f185676220469
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2023-12-14 13:07:24 +0100

    removed duplicate user models

--- backend/internal/models/platform.go
+++ backend/internal/models/platform.go
@@ -4,13 +4,6 @@ import (
     "github.com/google/uuid"
 )
 
-type User struct {
-    ID uuid.UUID `json:"id"`
-    Email string `json:"email"`
-    Username string `json:"username"`
-    Password string `json:"password_hash"`
-}
-
 type UserLikedAlbums struct {
     ID uuid.UUID `json:"id"`
     LibraryId uuid.UUID `json:"library_id"`
