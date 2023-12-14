d3513e3b16b304f4b39632c07b8cf9be7ffd1828
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2023-12-14 20:44:30 +0100

    added deezer scanner boiler plate

--- backend/internal/scanner/deezer.go
+++ backend/internal/scanner/deezer.go
@@ -1,16 +1,21 @@
 package scanner
 
 import (
+    "github.com/jbaxx/go-deezer/deezer"
 )
 
-func ScanDeezer(ID int) {
-    go ScanDeezerUserPlaylists()
-    go ScanDeezerUserAlbums()
-    go ScanDeezerUserTracks()
-    go ScanDeezerUserArtists()
+func ScanDeezer(id int) {
+    client := deezer.NewClient()
+    go ScanDeezerUserPlaylists(client, id)
+    go ScanDeezerUserAlbums(client, id)
+    go ScanDeezerUserTracks(client, id)
+    go ScanDeezerUserArtists(client, id)
 }
 
-func ScanDeezerUserPlaylists() {}
-func ScanDeezerUserAlbums() {}
-func ScanDeezerUserTracks() {}
-func ScanDeezerUserArtists() {}
+func ScanDeezerUserPlaylists(client *deezer.Client, id int) {
+
+}
+
+func ScanDeezerUserAlbums(client *deezer.Client, id int)  {}
+func ScanDeezerUserTracks(client *deezer.Client, id int)  {}
+func ScanDeezerUserArtists(client *deezer.Client, id int) {}
