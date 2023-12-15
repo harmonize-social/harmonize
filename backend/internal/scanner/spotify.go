78dceb9937fe6ac21c60a2544474b1b217848920
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2023-12-15 11:53:34 +0100

    fetching is clearer, artists are now all

--- backend/internal/scanner/spotify.go
+++ backend/internal/scanner/spotify.go
@@ -6,17 +6,18 @@ import (
     "github.com/zmb3/spotify"
 )
 
-func ScanSpotify(client spotify.Client) {
-    go ScanSpotifyUserPlaylists(&client)
-    go ScanSpotifyUserAlbums(&client)
-    go ScanSpotifyUserTracks(&client)
-    go ScanSpotifyUserArtists(&client)
+func FetchSpotify(client spotify.Client) {
+    playlists := FetchSpotifyUserPlaylists(&client)
+    go FetchSpotifyUserAlbums(&client)
+    go FetchSpotifyUserTracks(&client)
+    go FetchSpotifyUserArtists(&client)
 }
 
-func ScanSpotifyUserPlaylists(client *spotify.Client) {
+func FetchSpotifyUserPlaylists(client *spotify.Client) []spotify.SimplePlaylist {
     playlistsPage, err := client.CurrentUsersPlaylists()
     if err != nil {
         fmt.Printf("err current playlists")
+        return nil
     }
     playlists := playlistsPage.Playlists
     for true {
@@ -26,15 +27,14 @@ func ScanSpotifyUserPlaylists(client *spotify.Client) {
         }
         playlists = append(playlists, playlistsPage.Playlists...)
     }
-    for i, playlist := range playlists {
-        fmt.Printf("Playlist %d: %s (id: %s)\n\r", i, playlist.Name, playlist.ID.String())
-    }
+    return playlists
 }
 
-func ScanSpotifyUserAlbums(client *spotify.Client) {
+func FetchSpotifyUserAlbums(client *spotify.Client) []spotify.SavedAlbum {
     albumsPage, err := client.CurrentUsersAlbums()
     if err != nil {
         fmt.Printf("err current playlists")
+        return nil
     }
     albums := albumsPage.Albums
     for true {
@@ -44,19 +44,14 @@ func ScanSpotifyUserAlbums(client *spotify.Client) {
         }
         albums = append(albums, albumsPage.Albums...)
     }
-    for i, album := range albums {
-        fmt.Printf("Album %d: (", i)
-        for _, artist := range album.Artists {
-            fmt.Printf("%s", artist.Name)
-        }
-        fmt.Printf(") - %s\n\r", album.Name)
-    }
+    return albums
 }
 
-func ScanSpotifyUserTracks(client *spotify.Client) {
+func FetchSpotifyUserTracks(client *spotify.Client) []spotify.SavedTrack {
     savedTracksPage, err := client.CurrentUsersTracks()
     if err != nil {
         fmt.Printf("err current playlists")
+        return nil
     }
     tracks := savedTracksPage.Tracks
     for true {
@@ -66,24 +61,24 @@ func ScanSpotifyUserTracks(client *spotify.Client) {
         }
         tracks = append(tracks, savedTracksPage.Tracks...)
     }
-    for i, track := range tracks {
-        fmt.Printf("Track %d: (", i)
-        for _, artist := range track.Artists {
-            fmt.Printf("%s", artist.Name)
-        }
-        fmt.Printf(") - %s\n\r", track.Name)
-    }
+    return tracks
 }
 
-func ScanSpotifyUserArtists(client *spotify.Client) {
-    followedArtistsPage, err := client.CurrentUsersFollowedArtists()
-    if err != nil {
-        fmt.Printf("err current playlists")
+func FetchSpotifyUserArtists(client *spotify.Client) []spotify.FullArtist {
+    after := "-1"
+    limit := 50
+    total := 1000
+    var artists []spotify.FullArtist
+    for len(artists) < total {
+        list, err := client.CurrentUsersFollowedArtistsOpt(limit, after)
+        if err != nil {
+            fmt.Printf("%s", err)
+            break
+        }
+        for _, playlist := range list.Artists {
+            artists = append(artists, playlist)
+        }
+        after = list.Artists[len(list.Artists)-1].ID.String()
     }
-    err = nil
-    artists := followedArtistsPage.Artists
-    // TODO: Loop?
-    for i, artist := range artists {
-        fmt.Printf("Artist %d: %s\n\r", i, artist.Name)
-    }
+    return artists
 }
