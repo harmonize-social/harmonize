997d1bac95fca8ec4bb82d1798d7e91d58803f8a
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2023-12-16 18:22:08 +0100

    added connection model

--- backend/internal/models/user.go
+++ backend/internal/models/user.go
@@ -1,12 +1,22 @@
 package models
 
 import (
+    "time"
+
     "github.com/google/uuid"
 )
 
 type User struct {
-	ID uuid.UUID `json:"id"`
-	Email string `json:"email"`
-	Username string `json:"username"`
-	Password string `json:"password_hash"`
+    ID uuid.UUID `json:"id"`
+    Email string `json:"email"`
+    Username string `json:"username"`
+    Password string `json:"password_hash"`
+}
+
+type Connection struct {
+    ID uuid.UUID `json:"id"`
+    UserID uuid.UUID `json:"userId"`
+    AccessToken string `json:"accessToken"`
+    RefreshToken string `json:"refreshToken"`
+    Expiry time.Time `json:"expiry"`
 }
