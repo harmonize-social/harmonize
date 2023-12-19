fb22bf81ab668787785a9570d60b19dd66bcbd62
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2023-12-19 22:28:16 +0100

    added easy erroring and api responsing

--- backend/internal/models/api.go
+++ backend/internal/models/api.go
@@ -1,6 +1,30 @@
 package models
 
+import (
+    "encoding/json"
+    "fmt"
+    "net/http"
+)
+
 type ApiResponse struct {
-    Error string `json:"error:omitempty"`
-    Value interface{} `json:"value:omitempty"`
+    Error  string      `json:"error,omitempty"`
+    Result interface{} `json:"result,omitempty"`
+}
+
+func Result(w http.ResponseWriter, result interface{}) {
+    res := ApiResponse{
+        Result: result,
+    }
+
+    err := json.NewEncoder(w).Encode(res)
+    Error(w, http.StatusInternalServerError, fmt.Errorf("Error encoding JSON: %v", err).Error())
+}
+
+func Error(w http.ResponseWriter, code int, err string) {
+    res := ApiResponse{
+        Error: err,
+    }
+
+    w.WriteHeader(code)
+    json.NewEncoder(w).Encode(res)
 }
