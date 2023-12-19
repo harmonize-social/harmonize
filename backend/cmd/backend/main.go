cd526648b4b40588e5c8d6473b5748fd07dc3ecc
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2023-12-19 11:26:07 +0100

    prepare for middleware

--- backend/cmd/backend/main.go
+++ backend/cmd/backend/main.go
@@ -16,25 +16,38 @@ func main() {
     repositories.CreateConnection()
 
     router := mux.NewRouter()
-
-    mount(router, "/api/session", routers.SessionRouter())
-
-    // oauth
-    mount(router, "/api/oauth", routers.OAuthRouter())
-
-    // user.go routers
-    mount(router, "/api/user", routers.UserRouter())
-    mount(router, "/api/follow", routers.FollowRouter())
-
-    // interactions.go routers
-    mount(router, "/api/post", routers.PostRouter())
-    mount(router, "/api/like", routers.LikeRouter())
-    mount(router, "/api/comment", routers.CommentRouter())
-    mount(router, "/api/savedpost", routers.SavedPostRouter())
+    mount(router, "/api/v1/", unautherRoutes())
+    mount(router, "/api/v1/", authedRoutes())
 
     fmt.Println("Starting server on port 8080")
 
     log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func unautherRoutes() *mux.Router {
+    router := mux.NewRouter()
+    mount(router, "/session", routers.SessionRouter())
+    return router
+}
+
+func authedRoutes() *mux.Router {
+    router := mux.NewRouter()
+
+    mount(router, "/session", routers.SessionRouter())
+
+    // oauth
+    mount(router, "/oauth", routers.OAuthRouter())
+
+    // user.go routers
+    mount(router, "/user", routers.UserRouter())
+    mount(router, "/follow", routers.FollowRouter())
+
+    // interactions.go routers
+    mount(router, "/post", routers.PostRouter())
+    mount(router, "/like", routers.LikeRouter())
+    mount(router, "/comment", routers.CommentRouter())
+    mount(router, "/savedpost", routers.SavedPostRouter())
+    return router
 }
 
 func mount(r *mux.Router, path string, handler http.Handler) {
