9ec2396f95d029fff9a5994861d35fdd9517d797
Simon Verbandt <simon.verbandt@student.ehb.be>
2023-12-20 15:00:59 +0100

    update to profilepage

--- frontend/src/models/post.ts
+++ frontend/src/models/post.ts
@@ -9,13 +9,15 @@ export default class PostModel {
     type: string = 'type'
     type_id: string = 'type_id'
     content: SongModel | AlbumModel | PlaylistModel
+	likes: number;
 
-    constructor(data: {id: string, user_id: string, caption: string, type: string, type_id: string, content: SongModel | AlbumModel | PlaylistModel}){
+    constructor(data: {id: string, user_id: string, caption: string, type: string, type_id: string, content: SongModel | AlbumModel | PlaylistModel, likes: number}){
         this.id = data.id
         this.user_id = data.user_id
         this.caption = data.caption
         this.type = data.type
         this.type_id = data.type_id
         this.content = data.content
+        this.likes = data.likes
     }
 }
