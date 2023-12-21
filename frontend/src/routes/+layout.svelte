bc54137bbf18825d871d3cba7c34b2d2d0edce7a
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2023-12-21 14:03:23 +0100

    fix redirection issues

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,11 +1,22 @@
 <script>
 import { onMount } from "svelte";
 onMount(() => {
-    var url = window.location.href;
-    var path = url.replace('http://localhost:5173', '');
+    const url = window.location.href;
+    const host = url.match(/^https?:\/\/[^/]+/, '')[0];
+    const route = url.match(/^https?:\/\/[^/]+(.+)/)[1];
+    console.log(url)
+    console.log(host)
+    console.log(route)
     const token = localStorage.getItem('token');
-    if (!token && (path !== '/auth/login' && path !== '/auth/register')) {
-        window.location.replace('http://localhost:5173/auth/login');
+    if (!route) {
+        console.log('redirecting to /')
+        window.location.replace(host + '/');
+        return
+    }
+    if (!token && (route !== '/auth/login' && route !== '/auth/register')) {
+        console.log('redirecting to /auth/login')
+        window.location.replace(host + '/auth/login');
+        return
     }
 });
 </script>
