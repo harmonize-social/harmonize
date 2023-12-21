ba7ab0b37f2d18a9283681aac6e21f8882cc4d59
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2023-12-21 10:21:46 +0100

    platform models use platform sub models

--- backend/internal/models/platform.go
+++ backend/internal/models/platform.go
@@ -9,21 +9,21 @@ type PlatformArtist struct {
 type PlatformAlbum struct {
     ID string `json:"id,omitempty"`
     Title string `json:"title,omitempty"`
-    Artists []Artist `json:"artists,omitempty"`
-    Songs []Song `json:"songs,omitempty"`
+    Artists []PlatformArtist `json:"artists,omitempty"`
+    Songs []PlatformSong `json:"songs,omitempty"`
     MediaURL string `json:"mediaUrl,omitempty"`
 }
 
 type PlatformPlaylist struct {
     ID string `json:"id,omitempty"`
     Title string `json:"title,omitempty"`
-    Songs []Song `json:"songs,omitempty"`
+    Songs []PlatformSong `json:"songs,omitempty"`
     MediaURL string `json:"mediaUrl,omitempty"`
 }
 
 type PlatformSong struct {
     ID string `json:"id,omitempty"`
-    Artists []Artist `json:"artists,omitempty"`
+    Artists []PlatformArtist `json:"artists,omitempty"`
     Title string `json:"title,omitempty"`
     PreviewURL string `json:"previewUrl,omitempty"`
     MediaURL string `json:"mediaUrl,omitempty"`
