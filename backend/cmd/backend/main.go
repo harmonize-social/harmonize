8a52f14044e6c905dd2fc2c33728ed797a22995e
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2023-12-22 11:36:06 +0100

    removed printing since it doesn't print sub routers

--- backend/cmd/backend/main.go
+++ backend/cmd/backend/main.go
@@ -3,12 +3,10 @@ package main
 import (
     "backend/internal/repositories"
     "backend/internal/routers"
-    "fmt"
 
     "log"
     "net/http"
 
-    "github.com/gorilla/mux"
     "github.com/rs/cors"
 )
 
@@ -31,19 +29,7 @@ func main() {
         AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
     })
 
-    PrintRouter(router)
-
     handler := c.Handler(router)
 
     log.Fatal(http.ListenAndServe(":8080", handler))
-}
-
-func PrintRouter(router *mux.Router) {
-    router.Walk(func(route *mux.Route, router *mux.Router, ancestors []*mux.Route) error {
-        t, err := route.GetPathTemplate()
-        if err == nil {
-            fmt.Println("ROUTE:", t)
-        }
-        return nil
-    })
 }
