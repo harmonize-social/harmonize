a8ca8f5d165baa37d6f92afe8834974625b0ced2
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2023-12-23 11:17:24 +0100

    removed the oauth routes from router

--- backend/internal/routers/oauth.go
+++ backend/internal/routers/oauth.go
@@ -9,8 +9,6 @@ import (
 func OAuthRouter() *mux.Router {
     router := mux.NewRouter()
 
-    router.HandleFunc("/spotify", handlers.SpotifyURL).Methods("GET")
-    router.HandleFunc("/deezer", handlers.DeezerURL).Methods("GET")
     router.HandleFunc("/callback/deezer", handlers.DeezerCallback).Methods("GET")
     return router
 }
