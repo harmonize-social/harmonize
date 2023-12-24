2a9844d2bdb99b554c12c7c18b04ad64fa32b484
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2023-12-24 11:24:06 +0100

    moved scanner to platforms and refactoring

--- backend/internal/platforms/spotify.go
+++ backend/internal/platforms/spotify.go
@@ -1,6 +1,39 @@
-package scanner
+package platforms
 
-import ()
+import (
+    "backend/internal/repositories"
+    "context"
+
+    "github.com/google/uuid"
+    spotify "github.com/zmb3/spotify/v2"
+    spotifyauth "github.com/zmb3/spotify/v2/auth"
+    "golang.org/x/oauth2"
+)
+
+func SpotifyClientId(userId *uuid.UUID) (*spotify.Client, error) {
+    var token oauth2.Token
+    err := repositories.Pool.QueryRow(context.Background(), "SELECT access_token, refresh_token, expiry FROM connections JOIN libraries ON connections.id = libraries.connection_id WHERE user_id = $1 AND platform_id = $2", userId, "spotify").Scan(&token.AccessToken, &token.RefreshToken, &token.Expiry)
+    if err != nil {
+        return nil, err
+    }
+    auth := spotifyauth.New(
+        spotifyauth.WithClientID(repositories.SpotifyClientId),
+        spotifyauth.WithClientSecret(repositories.SpotifySecret),
+    )
+    newToken, err := auth.RefreshToken(context.Background(), &token)
+    if err != nil {
+        return nil, err
+    }
+    newAuth := spotifyauth.New()
+    httpClient := newAuth.Client(context.Background(), newToken)
+    client := spotify.New(httpClient)
+
+    return client, nil
+}
+
+func GetSpotifySong(songId string) (string, error) {
+    return "", nil
+}
 
 // func Spotify(client spotify.Client, connectionId uuid.UUID) {
 //     fmt.Printf("Create library\n\r")
