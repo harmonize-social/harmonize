247c7e8df139f57ab820fca3fac4333caabb870c
AbdelillahEl <abdelpro19@gmail.com>
2023-12-24 21:00:07 +0100

    Good changes

--- frontend/src/routes/profile/connection/+page.svelte
+++ frontend/src/routes/profile/connection/+page.svelte
@@ -3,46 +3,47 @@
 	import Panel from '../../../components/Panel.svelte';
 	import deezerIcon from '../../../lib/assets/deezer-logo-coeur.jpg';
 	import spotifyIcon from '../../../lib/assets/Spotify_App_Logo.svg.png';
-	import type ConnectionModel from '../../../models/connection';
 	import { onMount } from 'svelte';
-	let connections: ConnectionModel[] = [];
-	async function getConnections() {
+	async function getConnected() {
 		try {
-			const response: ConnectionModel[] = await get('/api/connections');
-			connections = response;
+			 const response: string[] = await get('/me/library/connected');
+			console.log(response);
 		} catch (e) {
 			throwError('Internal server error');
 		}
 	}
-	async function addConnection(platform: string) {
+	async function getUnconnected() {
 		try {
-			const response: ConnectionModel[] = await post(`/api/connections`, platform);
-			connections = response;
+			const response: Map<string, string> = await get('/me/library/unconnected');
+			console.log(response);
 		} catch (e) {
 			throwError('Internal server error');
 		}
 	}
-	onMount(getConnections);
+	onMount(() => {
+		getConnected();
+		getUnconnected();
+	})
 </script>
 
 <Panel title="Choose the platform to connect:">
 	<div class="container">
 		<div class="title">Select your preferred music platform:</div>
 		<div class="image-container">
-			<a href="/api/spotify" title="Connect with Spotify" on:click={() => addConnection('spotify')}>
+			<!-- <a href="/api/spotify" title="Connect with Spotify" on:click={() => addConnection('spotify')}>
 				<img src={spotifyIcon} alt="Spotify logo" />
 			</a>
 			<a href="/api/deezer" title="Connect with Deezer" on:click={() => addConnection('deezer')}>
 				<img src={deezerIcon} alt="Deezer logo" />
-			</a>
+			</a> -->
 		</div>
 		<div class="connected-platforms">
-			Your current connections:
+			<!-- Your current connections:
 			<ul>
 				{#each connections as connection}
 					<li>{connection.platform_name}</li>
 				{/each}
-			</ul>
+			</ul> -->
 		</div>
 	</div>
 </Panel>
