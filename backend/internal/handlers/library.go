0529932691a47d88f9e060e08cebc7a6a6b49a44
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2023-12-26 22:00:12 +0100

    fixed backend

--- backend/internal/handlers/library.go
+++ backend/internal/handlers/library.go
@@ -204,12 +204,35 @@ func DeleteConnectedPlatformHandler(w http.ResponseWriter, r *http.Request) {
         models.Error(w, http.StatusUnauthorized, "Malformed session")
         return
     }
-    _, err = repositories.Pool.Exec(r.Context(), "DELETE FROM libraries WHERE connection_id = (SELECT id FROM connections WHERE user_id = $1 AND platform_id = $2)", user.ID, platform)
+
+    if platform == "" {
+        models.Error(w, http.StatusBadRequest, "Missing platform")
+        return
+    }
+
+    // Library has platform_id and connection_id, connection has user_id, so you have to join to get connection_id
+    var connectionId uuid.UUID
+    err = repositories.Pool.QueryRow(r.Context(), "SELECT connection_id FROM libraries JOIN connections ON libraries.connection_id = connections.id WHERE user_id = $1 AND platform_id = $2", user.ID, platform).Scan(&connectionId)
     if err != nil {
+        fmt.Println("1:", err)
         models.Error(w, http.StatusInternalServerError, "Internal server error")
         return
     }
-    _, err = repositories.Pool.Exec(r.Context(), "DELETE FROM connections WHERE user_id = $1 AND platform_id = $2", user.ID, platform)
+
+    _, err = repositories.Pool.Exec(r.Context(), "DELETE FROM libraries WHERE user_id = $1 AND platform_id = $2", user.ID, platform)
+    if err != nil {
+        fmt.Println("2:", err)
+        models.Error(w, http.StatusInternalServerError, "Internal server error")
+        return
+    }
+
+    _, err = repositories.Pool.Exec(r.Context(), "DELETE FROM connections WHERE id = $1", connectionId)
+    if err != nil {
+        fmt.Println("2:", err)
+        models.Error(w, http.StatusInternalServerError, "Internal server error")
+        return
+    }
+
     models.Result(w, "Success")
 }
 
