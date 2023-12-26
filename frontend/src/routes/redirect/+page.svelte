bc8191cdc2ba2b6a1ff36ade8e27bfdaa82648cc
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2023-12-26 13:45:44 +0100

    fixed up the redirect page

--- frontend/src/routes/redirect/+page.svelte
+++ frontend/src/routes/redirect/+page.svelte
@@ -1,32 +1,37 @@
 <script lang="ts">
-let error = '';
+    import { onMount } from 'svelte';
+    import { get } from '../../fetch';
+    let error = '';
 
-onMount(() => {
-    queryParams = new URLSearchParams(window.location.search);
-    state = queryParams.get('state');
-    code = queryParams.get('code');
-    if (!code) {
-        error = 'No code provided';
-        return;
-    }
-    if (!state) {
-        error = 'No state provided';
-        return;
-    }
-    try {
-        let response = await get(`/oauth/callback/spotify?state=${state}&code=${code}`);
-        window.location.href = '/profile';
-    } catch (e) {
-        error = e.message;
-        return;
-    }
-});
+    onMount(async () => {
+        let queryParams = new URLSearchParams(window.location.search);
+        let state = queryParams.get('state');
+        let code = queryParams.get('code');
+        if (!code) {
+            error = 'No code provided';
+            return;
+        }
+        if (!state) {
+            error = 'No state provided';
+            return;
+        }
+        try {
+            let response = await get(`/oauth/callback/spotify?state=${state}&code=${code}`);
+            window.location.href = '/profile';
+        } catch (e) {
+            if (e instanceof Error) {
+                error = e.message;
+            } else {
+                error = 'Unknown error';
+            }
+            return;
+        }
+    });
 </script>
 
 <h1>Redirecting...</h1>
 <p>{error}</p>
 <p>If you do not get redirected automatically, <a href="/feed">Click here</a></p>
-
 
 <style>
 </style>
