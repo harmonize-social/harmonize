6127918715b9f2d2a163c6ad792a607ce487d753
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2024-01-02 22:08:51 +0100

    refactor: deezer callback reorganization for clarity

--- backend/internal/platforms/deezer.go
+++ backend/internal/platforms/deezer.go
@@ -13,7 +13,6 @@ import (
 
     "github.com/google/uuid"
     "github.com/markbates/goth/providers/deezer"
-    deezer2 "github.com/stayradiated/deezer"
 )
 
 type Tokens struct {
@@ -45,68 +44,56 @@ type DeezerAccessToken struct {
     AccessToken string `json:"access_token"`
 }
 
-func DeezerCallback(w http.ResponseWriter, r *http.Request) {
-    session := r.URL.Query().Get("state")
-    user, err := auth.GetUserFromSession(uuid.MustParse(session))
-    if err != nil {
-        fmt.Printf("Error: %s", err.Error())
-        models.Error(w, http.StatusUnauthorized, "Invalid token")
-        return
-    }
-    provider := DeezerProvider()
-    code := r.URL.Query().Get("code")
+func GetDeezerSession(provider *deezer.Provider, code string) (*deezer.Session, error) {
+    var token DeezerAccessToken
     url := "https://connect.deezer.com/oauth/access_token.php?app_id=" + provider.ClientKey + "&secret=" + provider.Secret + "&code=" + code + "&output=json"
     response, err := provider.Client().Get(url)
     if err != nil {
-        fmt.Fprintf(w, "err: %s", err)
-        return
+        return nil, err
     }
     defer response.Body.Close()
     body, err := io.ReadAll(response.Body)
-    var deezerToken DeezerAccessToken
-    json.Unmarshal(body, &deezerToken)
+    json.Unmarshal(body, &token)
     expiresAt := time.Now().Add(time.Hour * 24 * 365 * 100)
-    sessionActual := &deezer.Session{
+    session := &deezer.Session{
         AuthURL:     "",
-        AccessToken: deezerToken.AccessToken,
+        AccessToken: token.AccessToken,
         ExpiresAt:   expiresAt,
     }
-    response2, err := provider.Client().Get("https://api.deezer.com/user/me?access_token=" + sessionActual.AccessToken)
+    return session, nil
+}
+
+func DeezerCallback(w http.ResponseWriter, r *http.Request) {
+    state := r.URL.Query().Get("state")
+    provider := DeezerProvider()
+    user, err := auth.GetUserFromSession(uuid.MustParse(state))
     if err != nil {
-        fmt.Fprintf(w, "err: %s", err)
+        fmt.Println("Error: ", err.Error())
+        models.Error(w, http.StatusUnauthorized, "Invalid token")
         return
     }
-    defer response.Body.Close()
-    body2, err := io.ReadAll(response2.Body)
-    if err != nil {
-        fmt.Fprintf(w, "%s", err)
-        return
-    }
-    var deezerUser deezer2.User
-    json.Unmarshal(body2, &deezerUser)
-    if err != nil {
-        fmt.Fprintf(w, "%s", err)
-        return
-    }
+    code := r.URL.Query().Get("code")
+    session, err := GetDeezerSession(provider, code)
+
     connection := &models.Connection{
         ID:           uuid.New(),
         UserID:       user.ID,
-        AccessToken:  sessionActual.AccessToken,
+        AccessToken:  session.AccessToken,
         RefreshToken: "",
         Expiry:       time.Now().Add(time.Hour * 24 * 365 * 100),
     }
     sqlStatement := `INSERT INTO connections (id, user_id, access_token, refresh_token, expiry) VALUES ($1, $2, $3, $4, $5) RETURNING id`
     var connectionID uuid.UUID
-    err2 := repositories.Pool.QueryRow(context.Background(),
+    err = repositories.Pool.QueryRow(context.Background(),
         sqlStatement,
         connection.ID,
         connection.UserID,
         connection.AccessToken,
         connection.RefreshToken,
         connection.Expiry.Format(time.RFC3339)).Scan(&connectionID)
-    if err2 != nil {
+    if err != nil {
         fmt.Fprintf(w, "connection: %s\n\r", connection.UserID)
-        fmt.Fprintf(w, "Unable to execute the query. %s", err2)
+        fmt.Fprintf(w, "Unable to execute the query. %s", err)
     }
     sqlStatement = `
     INSERT INTO libraries (platform_id, id, connection_id) VALUES ('deezer', uuid_generate_v4(), $1) RETURNING id;
@@ -114,12 +101,8 @@ func DeezerCallback(w http.ResponseWriter, r *http.Request) {
     tag, err := repositories.Pool.Exec(context.Background(),
         sqlStatement,
         connectionID)
-    if err != nil {
+    if err != nil || tag.RowsAffected() == 0 {
         fmt.Printf("error: %v", err)
-        models.Error(w, http.StatusInternalServerError, "Internal server error")
-    }
-
-    if tag.RowsAffected() == 0 {
         models.Error(w, http.StatusInternalServerError, "Internal server error")
         return
     }
