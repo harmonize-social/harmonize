f127fc2964a83c7412f985db72fa57687f143d77
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2024-01-03 13:19:30 +0100

    fix: set type_specific_id in database for post

--- backend/internal/repositories/posts.go
+++ backend/internal/repositories/posts.go
@@ -10,8 +10,8 @@ import (
 func CreatePost(userID uuid.UUID, caption string, postType string, typeSpecificID uuid.UUID) (models.Post, error) {
     var post models.Post
 
-    sqlStatement := "INSERT INTO posts (id, user_id, caption, type, created_at) VALUES (uuid_generate_v4() ,$1, $2, $3, now()) RETURNING id, created_at, caption, type;"
-    err := Pool.QueryRow(context.Background(), sqlStatement, userID, caption, postType).Scan(&post.ID, &post.CreatedAt, &post.Caption, &post.Type)
+    sqlStatement := "INSERT INTO posts (id, user_id, caption, type, created_at, type_specific_id) VALUES (uuid_generate_v4() ,$1, $2, $3, now(), $4) RETURNING id, created_at, caption, type;"
+    err := Pool.QueryRow(context.Background(), sqlStatement, userID, caption, postType, typeSpecificID).Scan(&post.ID, &post.CreatedAt, &post.Caption, &post.Type)
 
     if err != nil {
         return post, err
