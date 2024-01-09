915751107a879a0127c5bb75caca84cc96bd948f
Simon Verbandt <simon.verbandt@student.ehb.be>
2024-01-09 23:57:34 +0100

    refactor: fix styling on followers and following dropdowns

--- frontend/src/routes/profile/[user]/+page.svelte
+++ frontend/src/routes/profile/[user]/+page.svelte
@@ -2,10 +2,9 @@
 	import ErrorPopup from '../../../components/ErrorPopup.svelte';
 	import Button from '../../../components/Button.svelte';
 	import NavBar from '../../../components/NavBar.svelte';
-	import { delete_, get, post , throwError} from '../../../fetch';
+	import { delete_, get, post, throwError } from '../../../fetch';
 	import type PostModel from '../../../models/post';
 	import { errorMessage } from '../../../store';
-
 
 	import Post from '../../../components/Post.svelte';
 	import type { PageData } from '../user/$types';
@@ -26,14 +25,13 @@
 	errorMessage.subscribe((value) => {
 		error = value;
 	});
-	
+
 	onMount(async () => {
 		errorMessage.set('');
 	});
-	$: if(username != old) {
+	$: if (username != old) {
 		refresh();
 	}
-	
 
 	async function refresh() {
 		posts = [];
@@ -48,7 +46,6 @@
 			posts = await get<PostModel[]>(`/posts?username=${username}`);
 		} catch (e) {
 			throwError('Error fetching user posts');
-
 		}
 	}
 
@@ -58,7 +55,6 @@
 			me = info.username;
 		} catch (e) {
 			throwError('Error fetching user info');
-
 		}
 	}
 
@@ -69,7 +65,6 @@
 			follows = followers.includes(username);
 		} catch (e) {
 			throwError('Error fetching user lists');
-
 		}
 	}
 
@@ -79,7 +74,6 @@
 			following = following.filter((user) => user !== username);
 		} catch (e) {
 			throwError('Error deleting user follow');
-
 		}
 	}
 
@@ -89,7 +83,6 @@
 			following = following.concat(username);
 		} catch (e) {
 			throwError('Error posting user follow');
-
 		}
 	}
 
@@ -123,41 +116,45 @@
 {#if username === me}
 	<div class="profile-container">
 		<div class="user-container">
-			<Button
-				action={() => (selectedList = selectedList === 'Followers' ? '' : 'Followers')}
-				buttonText="Followers: {followers.length}"
-			/>
-			{#if selectedList == 'Followers'}
-				<div class="followers-list-content">
-					<h4>Users who follow you:</h4>
-					{#if followers.length == 0}
-						<p>No followers</p>
-					{/if}
-					{#if followers.length > 0}
-						{#each followers as follower}
-							<a href="/profile/{follower}">{follower}</a>
-						{/each}
-					{/if}
-				</div>
-			{/if}
-			<Button
-				action={() => (selectedList = selectedList === 'Following' ? '' : 'Following')}
-				buttonText="Following: {following.length}"
-			/>
+			<div class="followers">
+				<Button
+					action={() => (selectedList = selectedList === 'Followers' ? '' : 'Followers')}
+					buttonText="Followers: {followers.length}"
+				/>
+				{#if selectedList == 'Followers'}
+					<div class="followers-list-content">
+						<h4>Users who follow you:</h4>
+						{#if followers.length == 0}
+							<p>No followers</p>
+						{/if}
+						{#if followers.length > 0}
+							{#each followers as follower}
+								<a href="/profile/{follower}">{follower}</a>
+							{/each}
+						{/if}
+					</div>
+				{/if}
+			</div>
+			<div class="following">
+				<Button
+					action={() => (selectedList = selectedList === 'Following' ? '' : 'Following')}
+					buttonText="Following: {following.length}"
+				/>
 
-			{#if selectedList == 'Following'}
-				<div class="following-list-content">
-					<h4>Users you follow:</h4>
-					{#if following.length == 0}
-						<p class="following-list">Not following anyone</p>
-					{/if}
-					{#if following.length > 0}
-						{#each following as follow}
-							<a href="/profile/{follow}">{follow}</a>
-						{/each}
-					{/if}
-				</div>
-			{/if}
+				{#if selectedList == 'Following'}
+					<div class="following-list-content">
+						<h4>Users you follow:</h4>
+						{#if following.length == 0}
+							<p class="following-list">Not following anyone</p>
+						{/if}
+						{#if following.length > 0}
+							{#each following as follow}
+								<a href="/profile/{follow}">{follow}</a>
+							{/each}
+						{/if}
+					</div>
+				{/if}
+			</div>
 			<Button buttonText="Library" action={() => goto('/profile/library')} />
 			<Button buttonText="Liked" action={() => goto('/profile/liked')}></Button>
 		</div>
@@ -240,7 +237,7 @@
 
 	.following-list-content,
 	.followers-list-content {
-		position: absolute;
+		position: fixed;
 		z-index: 1000;
 		border: black solid 1px;
 		border-radius: 1rem;
@@ -249,7 +246,7 @@
 		align-items: center;
 		justify-content: center;
 		padding: 0.5rem;
-		margin-top: 0.5rem;
+		margin-top: 0.2rem;
 		margin-left: 0rem;
 		background: rgb(234, 185, 255);
 		background: linear-gradient(
@@ -272,5 +269,9 @@
 	.followers-list-content a:hover,
 	.following-list-content a:hover {
 		text-transform: uppercase;
+	}
+
+	.follow_button{
+		margin: 0 auto;
 	}
 </style>
