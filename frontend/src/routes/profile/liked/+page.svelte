454eaa22eaf1dac03c6ace72a65725a9eb031471
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2024-01-09 15:54:30 +0100

    refactor: liked wasn't yet using new posts

--- frontend/src/routes/profile/liked/+page.svelte
+++ frontend/src/routes/profile/liked/+page.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
-    import Panel from "../../../components/Panel.svelte";
-    import NavBar from "../../../components/NavBar.svelte";
-    import Post from "../../../components/Post.svelte";
-	import {  get, throwError} from "../../../fetch";
-	import type PostModel from "../../../models/post";
-	import { onMount } from "svelte";
-	import ErrorPopup from "../../../components/ErrorPopup.svelte";
-	import { errorMessage } from '../../../store';
+    import Panel from '../../../components/Panel.svelte';
+    import NavBar from '../../../components/NavBar.svelte';
+    import Post from '../../../components/Post.svelte';
+    import { get, throwError } from '../../../fetch';
+    import type PostModel from '../../../models/post';
+    import { onMount } from 'svelte';
+    import ErrorPopup from '../../../components/ErrorPopup.svelte';
+    import { errorMessage } from '../../../store';
 
     let error: string = '';
     let liked_posts: PostModel[] = [];
@@ -14,68 +14,54 @@
 
     errorMessage.subscribe((value) => {
         error = value;
-    }); 
-    async function getLikedPosts(): Promise<PostModel[]>{
-        try{
+    });
+    async function getLikedPosts(): Promise<PostModel[]> {
+        try {
             const response: PostModel[] = await get('/me/saved');
             return response;
-        }catch(e){
+        } catch (e) {
             throwError('Internal server error');
             return [];
-        }finally{
+        } finally {
             loading = false;
         }
     }
 
     onMount(() => {
-		getLikedPosts().then((fetchedPosts) => {
-			liked_posts = fetchedPosts;
-		});
-	});
-
-	function onScroll(event: Event) {
-		const target = event.target as HTMLElement;
-		if (target.scrollHeight - target.scrollTop === target.clientHeight) {
-			loadMorePosts();
-		}
-	}
-	async function loadMorePosts() {
-		if (loading) return;
-		const morePosts = await getLikedPosts();
-		liked_posts = [...liked_posts, ...morePosts];
-	}
+        getLikedPosts().then((fetchedPosts) => {
+            liked_posts = fetchedPosts;
+        });
+    });
 </script>
+
 <NavBar current_page="/me/saved"></NavBar>
 <Panel title="Your liked posts">
-    <div class="liked-container" on:scroll={onScroll} >
         {#each liked_posts as post}
             <div class="post">
-                <Post content={post.content} caption={post.caption} likes={post.likeCount} id={post.id} typez={post.type} isLiked={true} isSaved={false}></Post>
+                <Post content={post} />
             </div>
         {/each}
         {#if loading}
             <p>Loading more posts...</p>
         {/if}
         {#if error}
-        <ErrorPopup message={error}></ErrorPopup>
+            <ErrorPopup message={error}></ErrorPopup>
         {/if}
-    </div>
-
-
 </Panel>
+
 <style>
     .liked-container {
-      width:40%;
-      height: 70%;
-      background-color: white;
-      border-radius: 10px;
-      margin: 0 auto;
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-      display: flex;
-      flex-direction: column;
-      align-items: center;
+        width: 40%;
+        height: 70%;
+        background-color: white;
+        border-radius: 10px;
+        margin: 0 auto;
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+        display: flex;
+        flex-direction: column;
+        align-items: center;
     }
-  </style>
+</style>
