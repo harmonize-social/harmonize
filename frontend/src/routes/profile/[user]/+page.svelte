4ffdee38bbd331f40f07efb9a83461d14b526961
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2024-01-09 14:47:43 +0100

    fix: show posts of user if they follow us, don't show if not

--- frontend/src/routes/profile/[user]/+page.svelte
+++ frontend/src/routes/profile/[user]/+page.svelte
@@ -1,163 +1,151 @@
 <script lang="ts">
-import ErrorPopup from '../../../components/ErrorPopup.svelte';
-import Button from '../../../components/Button.svelte';
-import Panel from '../../../components/Panel.svelte';
-import NavBar from '../../../components/NavBar.svelte';
-import {
-    delete_,
-    get,
-    post,
-    throwError
-} from '../../../fetch';
-import type PostModel from '../../../models/post';
-import Post from '../../../components/Post.svelte';
-import {
-    errorMessage
-} from '../../../store';
-import {
-    _fetchUserPosts
-} from './+page';
-import type {
-    PageData
-} from '../user/$types';
-import {
-    onMount
-} from 'svelte';
+    import ErrorPopup from '../../../components/ErrorPopup.svelte';
+    import Button from '../../../components/Button.svelte';
+    import Panel from '../../../components/Panel.svelte';
+    import NavBar from '../../../components/NavBar.svelte';
+    import { delete_, get, post, throwError } from '../../../fetch';
+    import type PostModel from '../../../models/post';
 
-let follows: boolean;
-let loading = false;
-let error = '';
-let text = '';
-export let data: PageData;
-export let posts: PostModel[] = data.user.posts;
-export let name: string = data.user.username;
+    import Post from '../../../components/Post.svelte';
+    import { errorMessage } from '../../../store';
+    import type { PageData } from '../user/$types';
+    import { onMount } from 'svelte';
 
-errorMessage.subscribe((value) => {
-    error = value;
-});
+    let followed: string[] = [];
+    let following: string[] = [];
+    let posts: PostModel[] = [];
+    let follows: boolean;
+    let loading = false;
+    let error = '';
+    $: text = follows ? 'Unfollow' : 'Follow';
+    export let data: PageData;
+    let username: any = data.user;
 
-async function isFollowing(username: string){
-    try {
-        const response = await get < any > (`/me/following?username=${username}`);
-        if(response.includes(username)){
-            follows = true;
-            text = 'Unfollow';
-        } else {
-            follows = false;
-            text = 'Follow';
+    errorMessage.subscribe((value) => {
+        error = value;
+    });
+
+    async function getPosts() {
+        try {
+            posts = await get<PostModel[]>(`/posts?username=${username}`);
+        } catch (error) {
+            throwError('Error fetching user posts');
         }
-
-    } catch (e) {
-        throwError('Error fetching user posts');
-        follows = false;
     }
-    return follows;
-}
-async function deleteFollow(username: string) {
-    try {
-        const response: string = await delete_ < string > (`/me/follow?username=${username}`);
-        return response;
-    } catch (e) {
-        throwError('Error deleting follow');
-        follows = true;
-    }
-}
-async function postFollow(username: string) {
-    try {
-        const response: string = await post < string, string > (`/me/follow?username=${username}`, username);
-        return response;
-    } catch (e) {
-        throwError('Error posting follow');
-        follows = false;
-    }
-}
 
-// function onScroll(event: Event) {
-//     const target = event.target as HTMLElement;
-//     if (target.scrollHeight - target.scrollTop === target.clientHeight) {
-//         loadMorePosts(name);
-//     }
-// }
-// async function loadMorePosts(name: string) {
-//     if (loading) return;
-//     const morePosts = await _fetchUserData(name);
-//     posts = [...posts, ...morePosts];
-// }
-
-let isClicked = false;
-const handleButtonClick = async () => {
-    if (follows) {
-        await deleteFollow(name);
-        text = 'Follow';
-        follows = false;
-    } else {
-        await postFollow(name);
-        text = 'Unfollow';
-        follows = true;
+    async function getFollowing() {
+        try {
+            followed = await get<string[]>(`/me/following`);
+            following = await get<string[]>(`/me/followers`);
+            follows = followed.includes(username);
+        } catch (e) {
+            throwError('Error fetching user posts');
+        }
     }
-    isClicked = !isClicked;
 
-};
-onMount(async () => {
-    await isFollowing(name);
-});
+    async function deleteFollow(username: string) {
+        try {
+            await delete_<string>(`/me/follow?username=${username}`);
+            following = following.filter((user) => user !== username);
+            follows = false;
+        } catch (e) {
+            throwError('Error deleting follow');
+        }
+    }
+
+    async function postFollow(username: string) {
+        try {
+            await post<any, string>(`/me/follow?username=${username}`, {});
+            following = following.filter((user) => user !== username);
+            follows = true;
+        } catch (e) {
+            throwError('Error posting follow');
+        }
+    }
+
+    // function onScroll(event: Event) {
+    //     const target = event.target as HTMLElement;
+    //     if (target.scrollHeight - target.scrollTop === target.clientHeight) {
+    //         loadMorePosts(name);
+    //     }
+    // }
+    // async function loadMorePosts(name: string) {
+    //     if (loading) return;
+    //     const morePosts = await _fetchUserData(name);
+    //     posts = [...posts, ...morePosts];
+    // }
+
+    let isClicked = false;
+    const handleButtonClick = async () => {
+        if (follows) {
+            await deleteFollow(username);
+        } else {
+            await postFollow(username);
+        }
+        isClicked = !isClicked;
+    };
+    onMount(async () => {
+        await getFollowing();
+        if (follows) {
+            await getPosts();
+        }
+    });
 </script>
 
 <!-- navbar -->
 <div class="nav">
-    <NavBar current_page={`/profile/${name}`}></NavBar>
+    <NavBar current_page={`/profile/${username}`}></NavBar>
 </div>
 <!-- profile -->
 <div class="profile-container">
-    <h2 class="username">{name}</h2>
+    <h2 class="username">{username}</h2>
 
     <!-- personal feed -->
     <div class="feed-container">
-        <Panel title={`${name}'s posts`}>
+        <div class="follow_button">
+            <Button buttonText={text} action={handleButtonClick}></Button>
+        </div>
+        <Panel title={`${username}'s posts`}>
             <div class="feed">
                 {#if posts.length === 0}
-                <p>{name} did not post yet!</p>
+                    <p>{username} did not post yet!</p>
                 {/if}
                 {#each posts as post}
-                <div class="post">
-                    <Post
-                        content={post.content}
-                        caption={post.caption}
-                        likes={post.likeCount}
-                        id={post.id}
-                        typez={post.type}
-                        isLiked={post.hasLiked}
-                        isSaved={post.hasSaved}
+                    <div class="post">
+                        <Post
+                            content={post.content}
+                            caption={post.caption}
+                            likes={post.likeCount}
+                            id={post.id}
+                            typez={post.type}
+                            isLiked={post.hasLiked}
+                            isSaved={post.hasSaved}
                         />
-                        </div>
-                        {/each}
-                        {#if loading}
-                        <p>Loading more posts...</p>
-                        {/if}
-                        {#if error}
-                        <ErrorPopup message={error}></ErrorPopup>
-                        {/if}
-                        </div>
-                        <div class="follow_button">
-                            <Button buttonText={text} action={handleButtonClick}></Button>
-                        </div>
-                        </Panel>
-                        </div>
-                        </div>
+                    </div>
+                {/each}
+                {#if loading}
+                    <p>Loading more posts...</p>
+                {/if}
+                {#if error}
+                    <ErrorPopup message={error}></ErrorPopup>
+                {/if}
+            </div>
+        </Panel>
+    </div>
+</div>
 
 <style>
-.profile-container {
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-}
+    .profile-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: flex-start;
+    }
 
-.username {
-    grid-area: username;
-}
+    .username {
+        grid-area: username;
+    }
 
-.feed {
-    display: flex;
-}
+    .feed {
+        display: flex;
+    }
 </style>
-
--- frontend/src/routes/profile/[user]/+page.ts
+++ frontend/src/routes/profile/[user]/+page.ts
@@ -1,26 +1,8 @@
-import { get } from '../../../fetch';
-import type PostModel from '../../../models/post';
 import type { PageLoad } from '../user/$types';
 
-
-
-export async function _fetchUserPosts(user: string): Promise<PostModel[]> {
-    try{
-        const response: PostModel[] = await get<PostModel[]>(`/posts?username=${user}`);
-        return response;
-    }catch(error){
-        console.log(error);
-        return [];
-    }
-}
-
 export const load: PageLoad = async ({ params }) => {
-    console.log(params);
-    const username = params.user;
-    return{ 
-        user: {
-            username,
-            posts: await _fetchUserPosts(params.user)
-        }
+    const user = params.user;
+    return{
+        user,
     }
 }
