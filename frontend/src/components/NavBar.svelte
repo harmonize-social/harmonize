dee6352b1c360b801adeb1cca9c183885ac36ec6
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2024-01-09 16:07:23 +0100

    refactor: added some indication of where we are

--- frontend/src/components/NavBar.svelte
+++ frontend/src/components/NavBar.svelte
@@ -5,12 +5,18 @@
     import { goto } from '$app/navigation';
     let all_pages = ['/feed', '/profile', '/profile/settings'];
     let all_texts = ['Feed', 'Profile', 'Settings'];
+    let current_page = window.location.pathname;
+    let current_index = all_pages.indexOf(current_page);
 </script>
 
 <nav class="navbar">
     <div class="logo" on:click={() => goto('/feed')}><Logo/></div>
     {#each all_pages as _, i}
-        <NavLink text={all_texts[i]} url={all_pages[i]}/>
+        {#if i == current_index}
+            <NavLink text={all_texts[i].toUpperCase()} url={all_pages[i]}/>
+        {:else}
+            <NavLink text={all_texts[i]} url={all_pages[i]}/>
+        {/if}
     {/each}
     <TextInputNav placeholder="Search"/>
 </nav>
