dc493189adb184f719cf565e64fc482bdb6c04ae
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2024-01-09 17:35:39 +0100

    refactor: cool fade bro

--- frontend/src/components/Playlist.svelte
+++ frontend/src/components/Playlist.svelte
@@ -1,50 +1,52 @@
 <!-- Playlist.svelte -->
 <script lang="ts">
-	import type PlaylistModel from '../models/playlist';
-	import PlaylistSong from './PlaylistSong.svelte';
-	export let content: PlaylistModel;
+    import type PlaylistModel from '../models/playlist';
+    import PlaylistSong from './PlaylistSong.svelte';
+    export let content: PlaylistModel;
 </script>
 
 <div class="playlist">
-	<h3>{content.title}</h3>
-	<img class="playlist-image" src={content.mediaUrl} alt="Playlist Cover" />
-	<div class="songs">
-		{#each content.songs as song, i}
-			<div class="song">
-				<PlaylistSong number={i + 1} content={song} />
-			</div>
-		{/each}
-	</div>
+    <h3>{content.title}</h3>
+    <img class="playlist-image" src={content.mediaUrl} alt="Playlist Cover" />
+    <div class="songs">
+        {#each content.songs as song, i}
+            <div class="song">
+                <PlaylistSong number={i + 1} content={song} />
+            </div>
+        {/each}
+    </div>
 </div>
 
 <style>
-	.playlist {
-		border: 1px solid #ccc;
-		border-radius: 10px;
-		width: 300px;
-		margin: 10px;
-		text-align: center;
-	}
+    .playlist {
+        border: 1px solid #ccc;
+        border-radius: 10px;
+        width: 300px;
+        margin: 10px;
+        text-align: center;
+    }
 
-	.playlist img {
-		width: 100px;
-		height: 100px;
-	}
+    .playlist img {
+        width: 100px;
+        height: 100px;
+    }
 
-	.songs {
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-		height: 200px;
-		overflow-y: scroll;
-		-webkit-scrollbar: none;
-	}
+    .songs {
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 200px;
+        overflow-y: scroll;
+        -webkit-scrollbar: none;
+        mask-image: linear-gradient(to bottom, transparent 0%, black 48px, black calc(100% - 48px), transparent 100%);
+        -webkit-mask-image: linear-gradient(to bottom, transparent 0%, black 48px, black calc(100% - 48px), transparent 100%);
+    }
 
-	.songs::-webkit-scrollbar {
-		display: none;
-	}
+    .songs::-webkit-scrollbar {
+        display: none;
+    }
 
-	.song {
-		margin: auto;
-	}
+    .song {
+        margin: auto;
+    }
 </style>
