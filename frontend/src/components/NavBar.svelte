164ba7c94c68557321966f3e4093268664089d7e
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2024-01-09 16:05:25 +0100

    refactor: new nav bar

--- frontend/src/components/NavBar.svelte
+++ frontend/src/components/NavBar.svelte
@@ -1,56 +1,33 @@
 <script lang="ts">
-	import TextInputNav from './TextInputNav.svelte';
-	import NavLink from './NavLink.svelte';
-	import Logo from './Logo.svelte';
-	import { page } from '$app/stores';
-	import { goto } from '$app/navigation';
-	export let current_page = $page.url.pathname;
-	export let url1: string = '#';
-	export let url2: string = '#';
-	export let text1: string = 'Feed';
-	export let text2: string = 'Settings';
-	switch (current_page) {
-		case '/me/feed':
-			url1 = '/profile';
-			url2 = '/profile/settings';
-			text1 = 'Profile';
-			text2 = 'Settings';
-			break;
-		case '/me/profile/settings':
-			url1 = '/feed';
-			url2 = '/profile';
-			text1 = 'Feed';
-			text2 = 'Profile';
-			break;
-		default:
-			url1 = '/feed';
-			url2 = '/profile/settings';
-			text1 = 'Feed';
-			text2 = 'Settings';
-			break;
-	}
+    import TextInputNav from './TextInputNav.svelte';
+    import NavLink from './NavLink.svelte';
+    import Logo from './Logo.svelte';
+    import { goto } from '$app/navigation';
+    let all_pages = ['/feed', '/profile', '/profile/settings'];
+    let all_texts = ['Feed', 'Profile', 'Settings'];
 </script>
 
 <nav class="navbar">
-	<div class="logo" on:click={() => goto('/feed')}><Logo></Logo></div>
-	<NavLink text={text1} url={url1} id={1}></NavLink>
-	<NavLink text={text2} url={url2} id={2}></NavLink>
-	<TextInputNav placeholder="Search"></TextInputNav>
+    <div class="logo" on:click={() => goto('/feed')}><Logo/></div>
+    {#each all_pages as _, i}
+        <NavLink text={all_texts[i]} url={all_pages[i]}/>
+    {/each}
+    <TextInputNav placeholder="Search"/>
 </nav>
 
 <style>
-	.navbar {
-		display: grid;
-		grid-template-columns: repeat(4, 1fr);
-		grid-template-areas: 'logo navlink1 navlink2 searchbox';
-		width: 45%;
-		height: 70px;
-		border: 1px solid black;
-		background-color: grey;
-		border-radius: 0 100px 100px 0;
-	}
+    .navbar {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        grid-template-areas: 'logo navlink1 navlink2 searchbox';
+        width: 45%;
+        height: 70px;
+        border: 1px solid black;
+        background-color: grey;
+        border-radius: 0 100px 100px 0;
+    }
 
-	.logo{
-		cursor: pointer;
-	}
+    .logo{
+        cursor: pointer;
+    }
 </style>
